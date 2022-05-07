c997ac057c0f7db0c66d2e32f7fb6c0bab62a877
vusal11010 <vusal11010@gmail.com>
2022-05-07 21:30:06 +0400

    Removed location option because of having only one

--- Customer Side/home.php
+++ Customer Side/home.php
@@ -2,30 +2,10 @@
         <form>
             <div class="pick-up">
                 <label>
-                    <h6>Pick-Up Location</h6>
-                    <select>
-                        <option value="">---</option>
-                        <option value="">Baku</option>
-                        <option value="">Shusha</option>
-                        <option value="">Radiator Springs</option>
-                    </select>
-                </label>
-                <label>
                     <h6>Pick-Up Date</h6>
                     <input type="date">
                 </label>
-            </div>
 
-            <div class="take-off">
-                <label>
-                    <h6>Take-Off Location</h6>
-                    <select>
-                        <option value="">---</option>
-                        <option value="">Baku</option>
-                        <option value="">Shusha</option>
-                        <option value="">Radiator Springs</option>
-                    </select>
-                </label>
                 <label>
                     <h6>Take-Off Date</h6>
                     <input type="date">
