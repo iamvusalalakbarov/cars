0b610f761d4fd432341cb5ed470c008b095831ae
vusal11010 <vusal11010@gmail.com>
2022-05-11 15:50:50 +0400

    Finished back-end for Contact page

--- Customer Side/contact.php
+++ Customer Side/contact.php
@@ -1,11 +1,32 @@
+<?php
+
+if (isset($_POST["submit"])) {
+    $firstname = $_POST["firstname"];
+    $surname = $_POST["surname"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $query = $db->prepare(
+        "INSERT INTO messages SET
+        firstname = ?,
+        surname = ?,
+        email = ?,
+        message = ?"
+        );
+    $insert = $query->execute([$firstname, $surname, $email, $message]);
+}
+
+?>
+
 <main class="contact">
         <h1>Contact Us</h1>
 
-        <form>
-            <input type="text" placeholder="First Name" required>
-            <input type="text" placeholder="Surname" required>
-            <input type="email" placeholder="E-mail" required>
-            <textarea placeholder="Your message..."></textarea>
+        <form method="POST">
+            <input type="text" placeholder="First Name" name="firstname" required>
+            <input type="text" placeholder="Surname" name="surname" required>
+            <input type="email" placeholder="E-mail" name="email" required>
+            <textarea placeholder="Your message..." name="message"></textarea>
+            <input type="hidden" name="submit" value="1">
             <button type="submit">Send</button>
         </form>
 </main>
